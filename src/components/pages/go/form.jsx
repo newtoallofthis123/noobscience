2dc36ce97cb3f9e8c3bea740edcd0fca7d281632
Noob Science <noobscience123@gmail.com>
2023-08-16 23:18:27 +0530

    Add NoobShort CLI

--- src/components/pages/go/form.jsx
+++ src/components/pages/go/form.jsx
@@ -19,64 +19,95 @@ export default function GoForm() {
     }
     return (
         <>
-        <form onSubmit={submit}>
-            <div className="md:mt-10 px-10">
-                <h1 className="text-5xl p-2">
-                    NoobShort
-                    </h1>
-                    <p className='text-lg p-2'>
+            <form onSubmit={submit}>
+                <div className="md:mt-10 px-10">
+                    <h1 className="text-5xl p-2">NoobShort</h1>
+                    <p className="text-lg p-2">
                         Because simplicity is the ultimate sophistication.
                     </p>
-                <div>
-                    <div className="mt-0 pt-4 p-2">
-                            <div style={{
-                                boxShadow: '0.3em 0.4em'
-                            }} className='border-2 p-2 border-black md:w-3/5 mt-2 mb-4 rounded-xl'>
+                    <div>
+                        <div className="mt-0 pt-4 p-2">
+                            <div
+                                style={{
+                                    boxShadow: '0.3em 0.4em',
+                                }}
+                                className="border-2 p-2 border-black md:w-3/5 mt-2 mb-4 rounded-xl"
+                            >
                                 <input
-                                    style={{
-                                        backgroundColor: '#f5f5f5',
-                                    }}
-                                type="url"
-                            className="w-5/6 p-2 border-r-2 border-black focus:outline-none text-lg"
-                                name="url"
-                                placeholder='Paste the Long Thing Here'
-                        />
+                                    autoComplete='off'
+                                    type="url"
+                                    className="w-5/6 p-2 border-r-2 border-black focus:outline-none text-lg"
+                                    name="url"
+                                    placeholder="Paste the Long Thing Here"
+                                />
                                 <button
-                            className="w-1/6 p-2 text-xl"
-                            type="submit"
-                        >
-                            Shorten It!
-                            </button>
-                        </div>
-                        <p>
-                            <span className="text-lg font-base py-3">
-                                Thank me later for this: {' '}
-                            </span>{' '}
-                            <a href={"https://noobscience.rocks/go/" + slug} className='text-lg font-base underline'>
-                              {slug}  
-                            </a> {
-                                slug && (
-                                    <span onClick={() => {
-                                navigator.clipboard.writeText("https://noobscience.rocks/go/" + slug);
-                                toast.success("Copied to Clipboard");
-                            }}><i className="bi bi-clipboard text-2xl font-base cursor-pointer"></i></span>
-                                )
-                            }
+                                    className="w-1/6 p-2 text-xl"
+                                    type="submit"
+                                >
+                                    Shorten It!
+                                </button>
+                            </div>
+                            <p>
+                                <span className="text-lg font-base py-3">
+                                    Thank me later for this:{' '}
+                                </span>{' '}
+                                <a
+                                    href={
+                                        'https://noobscience.rocks/go/' + slug
+                                    }
+                                    className="text-lg font-base underline"
+                                >
+                                    {slug}
+                                </a>{' '}
+                                {slug && (
+                                    <span
+                                        onClick={() => {
+                                            navigator.clipboard.writeText(
+                                                'https://noobscience.rocks/go/' +
+                                                    slug
+                                            );
+                                            toast.success(
+                                                'Copied to Clipboard'
+                                            );
+                                        }}
+                                    >
+                                        <i className="bi bi-clipboard text-2xl font-base cursor-pointer"></i>
+                                    </span>
+                                )}
                             </p>
-                            <h2 className='text-xl font-bold py-5'>
+                            <h2 className="text-xl font-bold py-2">
                                 Mandatory Dev Stuff
                             </h2>
-                            <p className='md:w-3/5 leading-8'>
-                                Hey! I am Ishan, the creator of NoobShort. NoobShort was initially a huge flask app with postgres.
-                                But, now, it is a simple React Component that uses MongoDB.
-                                This is mostly just to be used by me and my friends. But, if you want to use it, you can.
-                                However, don't post anything illegal.
-                                I regularly delete all the data from the database. So, don't expect your links to be there forever.
-                        </p>
+                            <p className="md:w-3/5 leading-8 text-base">
+                                Hey! I am Ishan, the creator of NoobShort.
+                                NoobShort was initially a huge flask app with
+                                postgres. But, now, it is a simple React
+                                Component that uses MongoDB. This is mostly just
+                                to be used by me and my friends. But, if you
+                                want to use it, you can. However, don't post
+                                anything illegal. I regularly delete all the
+                                data from the database. So, don't expect your
+                                links to be there forever.
+                            </p>
+                            <h2 className="text-xl font-bold py-2">
+                                NoobShort CLI!
+                            </h2>
+                            <p className="md:w-3/5 leading-8 text-base">
+                                Same shortener, but from the comfort of your
+                                terminal. Check it out at
+                                <a
+                                    href="https://github.com/newtoallofthis123/short_cli"
+                                    className="underline"
+                                >
+                                    {' '}
+                                    newtoallofthis123/short_cli
+                                </a>
+                                .
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </form>
+            </form>
         </>
     );
 }
